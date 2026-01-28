405622898ce9832e501b343947e49b43703c4599
jungukang <kjg0456@gmail.com>
2026-01-28 22:19:41 +0900

    feat: 환경분리 적용 db connection 에러 조치 필요

--- src/main/java/com/shlogis/groupware/groupwareapi/common/config/JasyptConfig.java
+++ src/main/java/com/shlogis/groupware/groupwareapi/common/config/JasyptConfig.java
@@ -1,30 +1,18 @@
 package com.shlogis.groupware.groupwareapi.common.config;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Jasypt 암호화 설정
+ *
+ * jasypt-spring-boot-starter가 application.yml의 설정을 자동으로 적용합니다.
+ *
+ * 사용법:
+ * - application.yml에서 jasypt.encryptor.password 설정
+ * - 암호화할 값은 ENC(암호화된값) 형식으로 사용
+ * - 환경변수 JASYPT_PASSWORD로 키 전달 가능
+ */
 @Configuration
 public class JasyptConfig {
-    private static final String KEY = "#shlogis";
-    private static final String ALGORITHM = "PBEWithMD5AndDES";
-    @Bean("jasyptStringEncryptor")
-    public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("shlogis");
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
-
-        String encrypted = encryptor.encrypt(KEY);
-        System.out.println("ENC(" + encrypted + ")");
-
-        return encryptor;
-    }
-
+    // jasypt-spring-boot-starter 자동 설정 사용
 }
